a6f079a73379502ed7c94982b0b951a07bbe2be8
Maximilian Weber <weber.maximilian@googlemail.com>
2014-05-09 10:09:26 +0200

    first commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clj-gzip "0.1.0"
+  :description "Compress and uncompress data with GZIP"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
